214fd917e973c6936a741c3e52980016ac3318eb
bilalmemon126 <bilalm3m9n@gmail.com>
2025-01-29 15:28:44 +0500

    some changes

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ let datetime = document.querySelector('.datetime');
 let timeZone = document.querySelector('.timeZone');
 
 
-function getPrayerTime(){
+function getPrayerTime() {
     let city = document.querySelector('#city').value;
     let country = document.querySelector('#country').value;
     let method = 3;
@@ -28,35 +28,48 @@ function getPrayerTime(){
     axios.get(`http://api.aladhan.com/v1/timingsByCity?city=${city}&country=${country}&method=${method}`)
         .then(function (response) {
             // handle success
-            console.log(response);
             f_prayer = fajr.innerHTML = response.data.data.timings.Fajr;
             d_prayer = dhuhr.innerHTML = response.data.data.timings.Dhuhr;
             a_prayer = asr.innerHTML = response.data.data.timings.Asr;
             m_prayer = maghrib.innerHTML = response.data.data.timings.Maghrib;
             i_prayer = isha.innerHTML = response.data.data.timings.Isha;
-            
+
             hijriMonth.innerHTML = response.data.data.date.hijri.month.ar;
             hijriYear.innerHTML = response.data.data.date.hijri.date;
             datetime.innerHTML = response.data.data.date.readable;
             timeZone.innerHTML = response.data.data.meta.timezone;
 
-            if(fajr >= f_prayer){
+
+            function toMinutes(timeString) {
+                let [hours, minutes] = timeString.split(':').map(Number);
+                return hours * 60 + minutes;
+            }
+
+            f_minutes = toMinutes(f_prayer)
+            d_minutes = toMinutes(d_prayer)
+            a_minutes = toMinutes(a_prayer)
+            m_minutes = toMinutes(m_prayer)
+            i_minutes = toMinutes(i_prayer)
+
+            time = toMinutes(moment().format('HH : mm'))
+            
+            if(time >= f_minutes && time < d_minutes){
                 comingtime.innerHTML = 'Dhuhr';
                 comingtime1.innerHTML = response.data.data.timings.Dhuhr;
             }
-            if(dhuhr >= d_prayer){
+            else if(time >= d_minutes && time < a_minutes){
                 comingtime.innerHTML = 'Asr';
                 comingtime1.innerHTML = response.data.data.timings.Asr;
             }
-            if(asr >= a_prayer){
+            else if(time >= a_minutes && time < m_minutes){
                 comingtime.innerHTML = 'Maghrib';
                 comingtime1.innerHTML = response.data.data.timings.Maghrib;
             }
-            if(maghrib >= m_prayer){
+            else if(time >= m_minutes && time < i_minutes){
                 comingtime.innerHTML = 'Isha';
                 comingtime1.innerHTML = response.data.data.timings.Isha;
             }
-            if(isha >= i_prayer){
+            else{
                 comingtime.innerHTML = 'Fajr';
                 comingtime1.innerHTML = response.data.data.timings.Fajr;
             }
